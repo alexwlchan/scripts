ff64d788b9b42095a26af65e1c76161397766785
Alex Chan <alex@alexwlchan.net>
2022-05-14 21:05:30 +0100

    Tidy up my script for installing Rust binaries
    *   Add more comments and documentation
    *   Make it generic so it can download multiple projects (but with aliases
        for command-line autocomplete)
    *   Use the GitHub Release Assets API

---
+++ install-safari.rs
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# Gets the latest version of safari.rs, the command-line tool I use
+# to get URLs from my running web browser.
+#
+# See https://github.com/alexwlchan/safari.rs
+
+set -o errexit
+set -o nounset
+
+_install-rust-bin safari.rs
