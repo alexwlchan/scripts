30d4bc7dc0e4c1e7ecd5a783fb079f5830502bdb
Alex Chan <alex@alexwlchan.net>
2023-11-26 10:15:59 +0000

    Fix a couple of things with my venv and tmpdir scripts

--- fish_functions/venv.fish
+++ fish_functions/venv.fish
@@ -3,16 +3,13 @@
 # This is to prevent me from making a very common mistake, which is
 # creating the venv and then immediately running "pip install" without
 # activating it first.
-#
-# I upgrade pip because otherwise I get warnings about it being
-# out-of-date, and that's annoying.
 function venv
-    python3 -m venv .venv
-    source .venv/bin/activate.fish
-
-    python3 -m pip install --upgrade pip
+    echo "Creating virtual environment in "(pwd)"/.venv"
+    python3 -m venv .venv --upgrade-deps
 
     if [ -f .git ]
         echo .venv >>.git/info/exclude
     end
+
+    source .venv/bin/activate.fish
 end
