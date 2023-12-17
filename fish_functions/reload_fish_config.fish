0aaab1cc9c5e6b796b2fc45c1cbc8643fdbc9c74
Alex Chan <alex@alexwlchan.net>
2023-12-17 11:05:59 +0000

    Make 'reload_fish_config' more thorough

--- fish_functions/reload_fish_config.fish
+++ fish_functions/reload_fish_config.fish
@@ -1,3 +1,11 @@
 function reload_fish_config
-    . ~/.config/fish/config.fish
+    source ~/.config/fish/config.fish
+
+    for file in $conf_dir/*.fish
+        source "$file"
+    end
+    
+    for file in ~/repos/scripts/fish_functions/*.fish
+        source "$file"
+    end
 end
