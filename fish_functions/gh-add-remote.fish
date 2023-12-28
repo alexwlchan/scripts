00300af0f65cbb9962c1bf87486e47f206b7acfc
Alex Chan <alex@alexwlchan.net>
2023-12-28 22:53:25 +0000

    Remove a now-unused Fish function

--- fish_functions/gh-add-remote.fish
+++
@@ -1,7 +0,0 @@
-# Within a GitHub repository, create a remote named 'alex' for a GitHub fork
-# of the same name.  Useful if I cloned before forking.
-function gh-add-remote
-    set origin_url (git remote get-url origin)
-    set repo_name (string split "/" "$origin_url" | tail -n 1)
-    git remote add alex "git@github.com:alexwlchan/$repo_name"
-end
