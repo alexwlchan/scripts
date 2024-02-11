e95f0ade940b4ac958cf0dcf499613a90f9dd960
Alex Chan <alex@alexwlchan.net>
2024-02-11 23:05:22 +0000

    Tweak my Fish config to use osascript directly

--- config.fish
+++ config.fish
@@ -47,8 +47,6 @@ function prepend_to_path
 end
 
 
-prepend_to_path ~/.cargo/bin
-
 prepend_to_path /Library/Frameworks/Python.framework/Versions/3.12/bin
 
 prepend_to_path ~/repos/scripts
@@ -105,7 +103,7 @@ end
 # Load macOS-specific utilities
 if [ (uname -s) = Darwin ]
     # Provide a convenient alias for the front URL in both browsers
-    alias furl="safari url"
+    alias furl="osascript -e 'tell application \"Safari\" to get URL of document 1'"
     alias gurl="osascript -e 'tell application \"Google Chrome\" to tell front window to get URL of tab (active tab index)'"
 
     # Get the URL of the frontmost GitHub page and clone it
