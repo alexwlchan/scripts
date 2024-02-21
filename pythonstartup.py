a20c62a47f546ca07ecbb1a9593edf720608f2bd
Alex Chan <alex@alexwlchan.net>
2024-02-21 20:40:43 +0000

    Remove an unused import; run black

--- pythonstartup.py
+++ pythonstartup.py
@@ -17,7 +17,6 @@ Features:
 
 import pprint
 import readline
-import rlcompleter
 import sys
 
 
@@ -43,4 +42,4 @@ sys.ps2 = "\033[1;34m... \033[0m"
 
 sys.displayhook = displayhook_pprint
 
-readline.parse_and_bind("tab: complete")
+readline.parse_and_bind('tab: complete')
