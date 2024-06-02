f16c510ab3203cb492897f0ff254bfdb63da7eed
Alex Chan <alex@alexwlchan.net>
2024-06-02 10:42:18 +0100

    pip_sync: if there isn't a virtualenv already, create one

--- fish_functions/pip_sync.fish
+++ fish_functions/pip_sync.fish
@@ -1,6 +1,11 @@
 function pip_sync --description "Make a virtualenv dependencies look like requirements.txt"
     pip_compile
 
+    # If there isn't a virtualenv already, create one
+    if test -z "$VIRTUAL_ENV"
+        venv
+    end
+
     if test \( -e dev_requirements.txt \)
         uv pip sync dev_requirements.txt
     else if test \( -e requirements.txt \)
